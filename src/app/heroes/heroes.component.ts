4f0fa26d6f2c6376befd82e0f267a85674b22af5
Javier Lete <jglete@gmail.com>
2019-05-02 19:59:11 +0200

    Servicio HeroService con getHeroes funcionando

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,28 @@
 import { HEROES } from '../mock-heroes';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-
-  heroes = HEROES;
-
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm'
-  };
-
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[];
+  
   selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes() : void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     console.log(hero);
