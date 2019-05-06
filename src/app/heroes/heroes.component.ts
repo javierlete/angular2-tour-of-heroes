fe200d0e45b0d320c0290ddcddebe22be91d0de3
Javier Lete <jglete@gmail.com>
2019-05-06 20:24:07 +0200

    HeroDetail con rutado y parámetro id

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,8 +12,6 @@ export class HeroesComponent implements OnInit {
   
   heroes: Hero[];
   
-  selectedHero: Hero;
-  
   constructor(private heroService: HeroService) { }
   
   ngOnInit(): void {
@@ -35,10 +33,5 @@ export class HeroesComponent implements OnInit {
   //recibirHeroes(heroes: Hero[]) : void {
   //  this.heroes = heroes;
   //}
-
-  onSelect(hero: Hero): void {
-    console.log(hero);
-    this.selectedHero = hero;
-  }
 }
 
