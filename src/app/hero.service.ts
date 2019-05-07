0d8febb45f8bc5b9f88757ca01cf05c427fc2671
Javier Lete <jglete@gmail.com>
2019-05-07 19:34:00 +0200

    getHero en HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,33 +19,35 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     this.log('fetching heroes');
-    
+
     return this.http.get<Hero[]>(this.heroesUrl).pipe(
       tap(_ => this.log('fetched heroes')),
       //tap(function() { this.log('fetched heroes');}),
       catchError(this.handleError<Hero[]>('getHeroes', []))
     );
   }
-  
-  private handleError<T> (operation = 'operation', result?: T) {
+
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-   
+
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-   
+
       // TODO: better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
-   
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
 
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.log(`fetched hero id=${id}`);
-    //return of(HEROES.find(hero => hero.id === id));
-    return of(null);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
